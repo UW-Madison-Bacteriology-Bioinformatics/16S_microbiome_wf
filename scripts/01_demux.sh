f413c794d4cbafd66bbf0e45b0c51ef192f1a0c2
jqin53 <jqin53@ap2002.chtc.wisc.edu>
2025-09-24 10:03:16 -0500

    fix barcode column name

--- scripts/01_demux.sh
+++ scripts/01_demux.sh
@@ -8,14 +8,14 @@ PROJECT="$2"
 echo "Importing..."
 qiime tools import \
   --type EMPPairedEndSequences \
-  --input-path /staging/${NETID}/${PROJECT}/input_outputs/00_pipeline_inputs/seqs \
+  --input-path /staging/${NETID}/${PROJECT}/input_outputs/00_pipeline_inputs/seqs-2 \
   --output-path original_demux.qza
 
 echo "Demultiplexing..."
 qiime demux emp-paired \
   --i-seqs original_demux.qza \
   --m-barcodes-file sample-metadata.tsv \
-  --m-barcodes-column barcodesequence \
+  --m-barcodes-column barcode-sequence \
   --o-per-sample-sequences demux.qza \
   --p-rev-comp-mapping-barcodes \
   --o-error-correction-details error.qza

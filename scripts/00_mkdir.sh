6b69280898468b6eaa373a1fded811ca6b77bb13
Patricia Tran <ptran5@wisc.edu>
2025-11-03 08:38:29 -0600

    improved terminal printout message

--- scripts/00_mkdir.sh
+++ scripts/00_mkdir.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
 #To run script, simply type bash 00_mkdir.sh into the terminal
 
+if [ "$#" -lt 2 ]; then
+  echo "Usage: bash 00_mkdir.sh NETID PROJECT" >&2
+  echo "Error: Not enough arguments provided. Provide your netID as the 1st argument, and your project name as the 2nd argument" >&2
+  exit 1
+fi
 
 # arguments
 NETID="$1"
 project="$2"
 
+echo "NetID is: ${NETID}"
+echo "Project name is: ${project}"
+
 staging="/staging/${NETID}"
 
-echo $staging
-echo $project
+echo "Folder will be created at: $staging/$project"
 
 mkdir -p $staging/$project/
 mkdir -p $staging/$project/00_pipeline_inputs/
@@ -24,3 +31,4 @@ mkdir -p $staging/$project/07_taxonomy/
 mkdir -p $staging/$project/08_ancombc/
 
 echo "Created folder structure for 16S rRNA pipeline at $staging/$project"
+echo "Completed"

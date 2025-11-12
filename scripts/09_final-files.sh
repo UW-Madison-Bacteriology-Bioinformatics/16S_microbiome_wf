42f36ee1a5f42a402c0b498ad1e3ea439aa8887c
Patricia Tran <ptran5@wisc.edu>
2025-11-12 11:30:38 -0600

    change file paths

--- scripts/09_final-files.sh
+++ scripts/09_final-files.sh
@@ -9,8 +9,8 @@ MY_PROJECT="/staging/$NETID/$PROJECT/"
 echo "The results will be saved here:  $MY_PROJECT"
 
 echo "Creating the otu_table.tsv file"
-qiime tools export --input-path $MY_PROJECT/02_dada2_qc/table.qza --output-path 09_final-files
-qiime tools export --input-path $MY_PROJECT/07_taxonomy/taxonomy.qza --output-path 09_final-files
-biom convert -i $MY_PROJECT/09_final-files/feature-table.biom -o 09_final-files/otu_table.tsv --to-tsv
+qiime tools export --input-path $MY_PROJECT/02_dada2_qc/table.qza --output-path $MY_PROJECT/09_final-files
+qiime tools export --input-path $MY_PROJECT/07_taxonomy/taxonomy.qza --output-path $MY_PROJECT/09_final-files
+biom convert -i $MY_PROJECT/09_final-files/feature-table.biom -o $MY_PROJECT/09_final-files/otu_table.tsv --to-tsv
 
 echo "Done"

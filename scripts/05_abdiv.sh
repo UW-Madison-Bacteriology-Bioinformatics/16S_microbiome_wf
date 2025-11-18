28ed43d1537c325712985f78799c938b098249aa
Patricia Tran <ptran5@wisc.edu>
2025-11-17 19:58:02 -0600

    multithread the cpus to make the 05 step faster. Also added ordination plots

--- scripts/05_abdiv.sh
+++ scripts/05_abdiv.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 set -e 
 COLUMN="$1"
+CPU="$2"
+
 echo "The group we are using is: ${COLUMN}"
+echo "using cpus: ${CPU}
 
 echo "Computing diversity metrics..."
 
@@ -10,49 +13,54 @@ qiime diversity core-metrics-phylogenetic \
   --i-table table.qza \
   --p-sampling-depth 1103 \
   --m-metadata-file sample-metadata.tsv \
-  --output-dir diversity-core-metrics-phylogenetic
-
+  --output-dir core-metrics-results \
+  --p-n-jobs-or-threads ${CPU}
 
 echo  "Testing for differences in alpha diversity between groups of samples..."
 
 qiime diversity alpha-group-significance \
-  --i-alpha-diversity diversity-core-metrics-phylogenetic/faith_pd_vector.qza \
+  --i-alpha-diversity core-metrics-results/faith_pd_vector.qza \
   --m-metadata-file sample-metadata.tsv \
-  --o-visualization faith-pd-group-significance.qzv
+  --o-visualization core-metrics-results/faith-pd-group-significance.qzv \
 
 qiime diversity alpha-group-significance \
-  --i-alpha-diversity diversity-core-metrics-phylogenetic/evenness_vector.qza \
+  --i-alpha-diversity core-metrics-results/evenness_vector.qza \
   --m-metadata-file sample-metadata.tsv \
-  --o-visualization evenness-group-significance.qzv
+  --o-visualization core-metrics-results/evenness-group-significance.qzv
 
 echo  "Testing differences in beta diversity between groups of samples..."
 
 qiime diversity beta-group-significance \
-  --i-distance-matrix diversity-core-metrics-phylogenetic/unweighted_unifrac_distance_matrix.qza \
+  --i-distance-matrix core-metrics-results/unweighted_unifrac_distance_matrix.qza \
   --m-metadata-file sample-metadata.tsv \
   --m-metadata-column ${COLUMN} \
-  --p-pairwise \
-  --o-visualization unweighted-unifrac-${COLUMN}-group-significance.qzv
+  --o-visualization core-metrics-results/unweighted-unifrac-${COLUMN}-significance.qzv \
+  --p-pairwise
 
+qiime diversity beta-group-significance \
+  --i-distance-matrix core-metrics-results/unweighted_unifrac_distance_matrix.qza \
+  --m-metadata-file sample-metadata.tsv \
+  --m-metadata-column ${COLUMN} \
+  --o-visualization core-metrics-results/unweighted-unifrac-${COLUMN}-group-significance.qzv \
+  --p-pairwise
+  
 # Emperor plots:
 qiime emperor plot \
   --i-pcoa core-metrics-results/unweighted_unifrac_pcoa_results.qza \
   --m-metadata-file sample-metadata.tsv \
   --p-custom-axes ${COLUMN} \
-  --o-visualization core-metrics-results/unweighted-unifrac-emperor-days-since-experiment-start.qzv
+  --o-visualization core-metrics-results/unweighted-unifrac-emperor-${COLUMN}.qzv
 
 qiime emperor plot \
   --i-pcoa core-metrics-results/bray_curtis_pcoa_results.qza \
   --m-metadata-file sample-metadata.tsv \
   --p-custom-axes ${COLUMN} \
-  --o-visualization core-metrics-results/bray-curtis-emperor-days-since-experiment-start.qzv
+  --o-visualization core-metrics-results/bray-curtis-emperor-${COLUMN}.qzv
 
+ls core-metrics-results
 
-ls diversity-core-metrics-phylogenetic
-
-tar cfv diversity-core-metrics-phylogenetic.tar.gz diversity-core-metrics-phylogenetic
+tar cfv diversity-core-metrics-phylogenetic.tar.gz core-metrics-results
 
 ls
 
 echo "Job 05 Complete!"
-

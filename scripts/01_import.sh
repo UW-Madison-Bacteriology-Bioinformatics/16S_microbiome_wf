ae1337305488a0068ab510d0dfaf2288d1b67796
Patricia Tran <ptran5@wisc.edu>
2026-02-04 18:04:00 -0600

    using a zipped input folder from home rather than a direct path to reads

--- scripts/01_import.sh
+++ scripts/01_import.sh
@@ -10,10 +10,17 @@ echo ${PROJECT}
 
 echo "Container launched successfully..."
 
+echo "folder contents: $(ls)"
 MANIFEST_SRC="fastq-manifest.txt"
 MANIFEST_TMP="manifest_expanded.txt"
 
 envsubst < "$MANIFEST_SRC" > "$MANIFEST_TMP"
+
+echo "Unzipping sequences"
+tar xfv seqs.tar.gz
+
+echo "Path to working directory: $(pwd)"
+echo "Contents of seqs folder: $(ls seqs)"
 
 echo "Importing with QIIME tools import..."
 qiime tools import --type 'SampleData[PairedEndSequencesWithQuality]' \
